24d0cc4392d0bebe1bec8e4e3cccc4cda2dc62ca
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 20:44:59 +0100

    add SearchForm to TopBar

--- src/core/App/Layout/TopBar/TopBar.js
+++ src/core/App/Layout/TopBar/TopBar.js
@@ -1,7 +1,17 @@
+import { useLocation } from "react-router";
+
+import SearchForm from "common/SearchForm";
+
 const TopBar = () => {
+  const { pathname } = useLocation();
+
   return (
     <header>
       Header
+      {
+        pathname !== "/" &&
+        <SearchForm />
+      }
     </header>
   );
 };
