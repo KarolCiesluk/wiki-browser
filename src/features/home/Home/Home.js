852a19fa444ac6f60b931ebadf422ecc7c25e811
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 12:14:49 +0100

    turn input into controlled field

--- src/features/home/Home/Home.js
+++ src/features/home/Home/Home.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [inputValue, setInputValue] = useState();
+
+  const handleInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
   return (
     <div>
       <form>
         <label>
-          <input placeholder="Search Wikipedia" />
+          <input
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Search Wikipedia"
+          />
           <button type="submit">Search</button>
         </label>
       </form>
