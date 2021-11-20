339d54de7ffee0aa14f3ca7a292a3a0596ccfb7b
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 15:18:33 +0100

    create SearchForm with formik

---
+++ src/features/searchForm/SearchForm/SearchForm.js
@@ -0,0 +1,38 @@
+import { ErrorMessage, Form, Formik } from "formik";
+
+import { TextInput } from "./TextInput";
+import { validationSchema } from "./validationSchema";
+import { inputValueName } from "./inputValueName";
+import { useHandleSubmit } from "./useHandleSubmit";
+
+const SearchForm = () => {
+  const handleSubmit = useHandleSubmit();
+
+  return (
+    <Formik
+      initialValues={{
+        [inputValueName]: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <>
+        <Form>
+
+          <TextInput
+            aria-label="Search Wikipedia"
+            name={inputValueName}
+            type="text"
+            placeholder="Search Wikipedia"
+          />
+          <ErrorMessage name={inputValueName} />
+
+          <button type="submit">Search</button>
+
+        </Form>
+      </>
+    </Formik>
+  );
+};
+
+export default SearchForm;
