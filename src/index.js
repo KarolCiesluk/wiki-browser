7f21e14309e63b52c167b78309c6436b6f0f7e84
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 10:46:40 +0100

    add styled-normalize

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App, GlobalStyle } from 'core';
+import { Normalize } from 'styled-normalize';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <GlobalStyle />
     <App />
   </React.StrictMode>,
