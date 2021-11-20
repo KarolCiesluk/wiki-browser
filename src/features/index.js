22a40634f259599a87bee0a4e1141856b1fcc5a2
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 20:46:07 +0100

    add Home export to index file in features

--- src/features/index.js
+++ src/features/index.js
@@ -2,3 +2,4 @@ export { default as SearchForm } from "./searchForm";
 export { default as Article } from "./article";
 export { default as ArticleList } from "./articleList";
 export { default as SearchHistory } from "./searchHistory";
+export { default as Home } from "./home";
