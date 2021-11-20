a05d8fcda10a99e27803ff60d1656fb7297ae373
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 11:52:03 +0100

    fix typos in exports

--- src/features/index.js
+++ src/features/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from "./home";
 export { default as Article } from "./article";
-export { default as ArticleList } from "./ArticleList";
-export { default as SearchHistory } from "./SearchHistory";
+export { default as ArticleList } from "./articleList";
+export { default as SearchHistory } from "./searchHistory";
