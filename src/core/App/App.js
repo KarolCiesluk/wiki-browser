aa3b4fc05e237246ea9dba63b5a7719ee52ea3da
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-20 10:11:35 +0100

    move App.js to core folder

---
+++ src/core/App/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <form>
+        <label>
+          <input placeholder="Search Wikipedia" />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
