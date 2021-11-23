802f605f625404b2495d465f31989415a13d2990
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-23 15:54:18 +0100

    add new Styling to TopBar

--- src/core/App/Layout/TopBar/styled.js
+++ src/core/App/Layout/TopBar/styled.js
@@ -1,6 +1,28 @@
 import styled from "styled-components";
+import { FaWikipediaW } from 'react-icons/fa';
 
 export const Header = styled.header`
   border-bottom: 1px solid #EAEEF3;
-  padding: 20px 0;
+  padding: 14px 0;
 `;
+
+export const TopBarGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr;
+  grid-gap: 30px;
+  align-items: center;
+  width: 100%;
+`;
+
+export const StyledWikiIcon = styled(FaWikipediaW)`
+  width: 56px;
+  height: auto;
+  color: #1483fb;
+`;
+
+export const FormWrapper = styled.div`
+  align-self: start;
+  justify-self: end;
+`;
+
