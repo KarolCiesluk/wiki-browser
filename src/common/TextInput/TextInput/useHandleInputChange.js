634290fc35fac6fe18f0900e203670448f6b6f3f
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 13:08:23 +0100

    simplify useHandleInputChange hook

--- src/common/TextInput/TextInput/useHandleInputChange.js
+++ src/common/TextInput/TextInput/useHandleInputChange.js
@@ -1,26 +1,19 @@
-import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 import { actions } from "../textInputSlice";
 
-const SEARCH_DELAY = 500;
-
 export const useHandleInputChange = onChange => {
-  const timeoutRef = useRef();
   const dispatch = useDispatch();
 
   return (event) => {
-    clearTimeout(timeoutRef.current);
     onChange(event);
 
     const value = event.target.value;
 
-    timeoutRef.current = setTimeout(() => {
-      if (value && value.trim()) {
-        dispatch(actions.fetch(value));
-      } else {
-        dispatch(actions.clear());
-      }
-    }, SEARCH_DELAY);
+    if (value && value.trim()) {
+      dispatch(actions.fetch(value));
+    } else {
+      dispatch(actions.clear());
+    }
   };
 };
