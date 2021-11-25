7f28589e733b36be3adf2d55451c452f5a5bb3c4
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 11:11:38 +0100

    use getListData in getSuggestions

--- src/common/TextInput/getSuggestions.js
+++ src/common/TextInput/getSuggestions.js
@@ -1,11 +1,9 @@
-const API_URL = "https://en.wikipedia.org/w/rest.php/v1/";
+import { getListData } from "common/getListData";
 
 export const getSuggestions = async (value) => {
-  const response = await fetch(`${API_URL}search/title?q=${value}&limit=2`);
-
-  if (!response.ok) {
-    new Error(response.statusText);
-  }
-
-  return await response.json();
+  return await getListData({
+    value,
+    endpoint: "search/title",
+    limit: 3
+  });
 };
