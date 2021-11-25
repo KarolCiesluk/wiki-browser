22147759ec4eb6a9228bf9dce490b6aeda2f25a4
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 13:08:02 +0100

    add SEARCH_DELAY to watchFetchSuggestions

--- src/common/TextInput/textInputSaga.js
+++ src/common/TextInput/textInputSaga.js
@@ -2,9 +2,12 @@ import { listSaga } from "common/listSaga";
 import { getSuggestions } from "./getSuggestions";
 import { actions } from "./textInputSlice";
 
+const SEARCH_DELAY = 500;
+
 export function* watchFetchSuggestions() {
   yield listSaga({
     actions,
     getListData: getSuggestions,
+    searchDelay: SEARCH_DELAY,
   });
 }
