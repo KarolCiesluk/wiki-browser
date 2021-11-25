3c609e8b01148ec3640a518a9875a7742697f3be
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 13:08:42 +0100

    add DEMO_DELAY to watchFetchArticles

--- src/features/articleList/articleListSaga.js
+++ src/features/articleList/articleListSaga.js
@@ -2,9 +2,12 @@ import { actions } from "./articleListSlice";
 import { listSaga } from "common/listSaga";
 import { getArticles } from "./getArticles";
 
+const DEMO_DELAY = 200;
+
 export function* watchFetchArticles() {
   yield listSaga({
     actions,
     getListData: getArticles,
+    searchDelay: DEMO_DELAY,
   });
 }
