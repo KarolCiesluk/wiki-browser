d1b5647a62cf104661210558753a96e4b6eab6ac
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 12:24:25 +0100

    import watch FetchArticles to rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,9 +1,11 @@
 import { all } from "redux-saga/effects";
 
-import { watchFetchSuggestions } from "common/TextInput/inputSaga";
+import { watchFetchSuggestions } from "common/textInput/textInputSaga";
+import { watchFetchArticles } from "features/articleList/articleListSaga";
 
 export function* rootSaga() {
   yield all([
     watchFetchSuggestions(),
+    watchFetchArticles(),
   ]);
 };
