5d19d409fc024059acfeb2a0200b5cae43ac5f5e
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-25 14:42:34 +0100

    navigate to home page when no value

--- src/features/articleList/ArticleList/useFetchOnPageLoad.js
+++ src/features/articleList/ArticleList/useFetchOnPageLoad.js
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 
 export const useFetchOnPageLoad = ({ fetchAction, value }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(fetchAction(value));
+    value ?
+      dispatch(fetchAction(value)) :
+      navigate("/");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value]);
 };
