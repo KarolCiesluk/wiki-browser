29265ad23495a04a98b55019508fc88a8f2ed6eb
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-26 11:09:00 +0100

    add ArticleList page and styled-components

--- src/features/articleList/ArticleList/ArticleList.js
+++ src/features/articleList/ArticleList/ArticleList.js
@@ -1,8 +1,90 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { useClearDataOnLeave } from "common/useClearDataOnLeave";
+import { searchQueryName } from "common/searchQueryName";
+import { selectors, actions } from "../articleListSlice";
+import { useQueryParameter } from "./useQueryParameter";
+import { useFetchOnPageLoad } from "./useFetchOnPageLoad";
+import {
+  Image,
+  ImageWrapper,
+  List,
+  ListItem,
+  StyledLink,
+  TextWrapper,
+  Title
+} from "./styled";
+import { Header } from "common";
+import { Content } from "./Content";
+import placeholder from "./placeholder.svg";
+
 const ArticleList = () => {
+  const articles = useSelector(selectors.selectData);
+  const status = useSelector(selectors.selectStatus);
+
+  const query = useQueryParameter(searchQueryName);
+
+  const articlesLength = articles?.length;
+
+  useFetchOnPageLoad({
+    fetchAction: actions.fetch,
+    value: query,
+  });
+
+  useEffect(() => {
+    console.log("articles:", articles, "status:", status);
+  }, [articles, status]);
+
+  useClearDataOnLeave({ clearAction: actions.clear });
+
+  const getTitleText = () => {
+    if (!!articlesLength) {
+      return `Search results for "${query}" (${articlesLength}):`;
+    }
+
+    return `Sorry, there are no results for "${query}"`;
+  };
+
   return (
-    <div>
-      Article List
-    </div>
+    <Content status={status}>
+
+      <Header>
+        {getTitleText()}
+      </Header>
+
+      {
+        !!articlesLength &&
+        <List>
+          {articles.map(article =>
+            <ListItem key={article.id}>
+              <StyledLink
+                to={`/article/${article.id}`}
+              >
+                <ImageWrapper url={article.thumbnail?.url}>
+                  <Image
+                    src={article.thumbnail?.url || placeholder}
+                    alt=""
+                  />
+                </ImageWrapper>
+
+                <TextWrapper>
+                  <Title>
+                    {article.title}
+                  </Title>
+
+                  <p>
+                    {article.description}
+                  </p>
+                </TextWrapper>
+
+              </StyledLink>
+            </ListItem>
+          )}
+        </List>
+      }
+
+    </Content>
   );
 };
 
---
+++ src/features/articleList/ArticleList/styled.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(205px, 1fr));
+  grid-gap: 16px;
+
+  @media(max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const ListItem = styled.li`
+  border-radius: 4px;
+  box-shadow:
+    rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  transition: transform 0.2s, color 0.2s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    background-color: #1976d217;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  display: grid;
+  grid-template-rows: 200px 1fr;
+  height: 100%;
+  padding: 16px;
+  text-decoration: none;
+  color: inherit;
+
+  @media(max-width: 500px) {
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 8px;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 200px;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+`;
+
+export const TextWrapper = styled.div`
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  word-break: word-break;
+
+  @media(max-width: 700px) {
+    font-size: 20px;
+  }
+`;
