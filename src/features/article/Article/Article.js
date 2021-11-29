dd6210975b19cf0f1c5f1ece560534dfac9f03cb
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-29 16:51:49 +0100

    import useParams from react-router-dom

--- src/features/article/Article/Article.js
+++ src/features/article/Article/Article.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const Article = () => {
+  const { articleID } = useParams();
+
   return (
     <div>
-      Article Page
+      {`Article no. ${articleID}`}
     </div>
   );
 };
