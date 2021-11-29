b8305227d5da702882f2c43ad774161fd2ece669
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-29 15:33:38 +0100

    delete DEMO_DELAY from articleListSaga

--- src/features/articleList/articleListSaga.js
+++ src/features/articleList/articleListSaga.js
@@ -2,12 +2,9 @@ import { actions } from "./articleListSlice";
 import { listSaga } from "common/listSaga";
 import { getArticles } from "./getArticles";
 
-const DEMO_DELAY = 200;
-
 export function* watchFetchArticles() {
   yield listSaga({
     actions,
     getListData: getArticles,
-    searchDelay: DEMO_DELAY,
   });
 }
