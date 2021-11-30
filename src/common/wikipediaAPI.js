074c0d1487365cda63f7e3f8298689118d95b4eb
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-30 10:50:30 +0100

    add getWikipediaData, getArticleData and rename file to wikipediaAPI

---
+++ src/common/wikipediaAPI.js
@@ -0,0 +1,29 @@
+const getWikipediaData = async ({ language = "en", endpoint, queryParams = "" }) => {
+  const API_URL = `https://${language}.wikipedia.org/w/rest.php/v1/`;
+
+  const response = await fetch(`${API_URL}${endpoint}${queryParams}`);
+
+  if (!response.ok) {
+    new Error(response.statusText);
+  }
+
+  return await response.json();
+};
+
+export const getListData = async ({ query, endpoint, limit }) => {
+  const params = new URLSearchParams();
+  params.set("q", query);
+  params.set("limit", limit);
+
+  return await getWikipediaData({
+    endpoint,
+    queryParams: `?${params.toString()}`,
+  });
+};
+
+export const getArticleData = async ({ title, language, endpoint }) => {
+  return await getWikipediaData({
+    endpoint: `page/${title}${endpoint}`,
+    language,
+  });
+};
