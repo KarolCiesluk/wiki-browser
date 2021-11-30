f61a28b0d329446a382617b20b5f8960eae16599
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-30 11:18:31 +0100

    delete unnecessary comments and add payload to dependency array

--- src/common/useFetchOnPageLoad.js
+++ src/common/useFetchOnPageLoad.js
@@ -1,15 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router";
 
 export const useFetchOnPageLoad = ({ fetchAction, payload }) => {
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
 
   useEffect(() => {
-    // value ?
-      dispatch(fetchAction(payload));
-      // navigate("/");
+    dispatch(fetchAction(payload));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [payload]);
 };
