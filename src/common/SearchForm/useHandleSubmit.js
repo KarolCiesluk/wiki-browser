d1bfb06323439d9afc9446083aa5d868bc789fa0
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-11-30 11:19:26 +0100

    simplify useHandleSubmit hook

--- src/common/SearchForm/useHandleSubmit.js
+++ src/common/SearchForm/useHandleSubmit.js
@@ -1,21 +1,13 @@
 import { useNavigate } from "react-router";
 
-import { searchQueryName } from "common/searchQueryName";
-import { buildQueryString } from "./buildQueryString";
-
 export const useHandleSubmit = (inputValueName) => {
   const navigate = useNavigate();
 
   return (values) => {
-    const value = values[inputValueName].trim();
+    const query = values[inputValueName].trim();
 
-    if (value) {
-      const queryString = buildQueryString({
-        key: searchQueryName,
-        value,
-      });
-
-      navigate(`articles?${queryString}`);
+    if (query) {
+      navigate(`articles/${query}`);
     }
   };
 };
