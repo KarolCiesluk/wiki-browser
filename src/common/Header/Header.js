42fe2cd25f29f724c2efc803ea2bbca6d48dc27d
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-01 10:43:08 +0100

    update Header styling

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 const Header = styled.h1`
-  display: inline-block;
-  color: #0e1111;
-  padding-bottom: 4px;
+  font-weight: normal;
 
   @media(max-width: 700px) {
     font-size: 26px;
