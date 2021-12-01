985255db5096d61740b800bbc93fda3ff40ad987
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-01 10:41:39 +0100

    add extra styling to GlobalStyle

--- src/core/GlobalStyle.js
+++ src/core/GlobalStyle.js
@@ -1,17 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    html {
-        box-sizing: border-box;
-    }
+  html {
+    box-sizing: border-box;
+  }
 
-    *, ::after, ::before {
-        box-sizing: inherit;
-    }
+  *, ::after, ::before {
+    box-sizing: inherit;
+    max-width: 100%;
+  }
 
-    body {
-        font-family: 'Roboto', sans-serif;
+  body {
+    font-family: 'Roboto', sans-serif;
+    color: #0e1111;
+
+    @media (max-width: 400px) {
+      font-size: 14px;
     }
+  }
+
+  table {
+    display: block;
+    overflow: auto;
+  }
 `;
 
 export default GlobalStyle;
