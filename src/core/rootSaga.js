c64bcc63e13a8cbfaae07081d2ab3c3c13ef20c3
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-03 10:13:20 +0100

    add watchSearchHistoryChange to rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -3,11 +3,13 @@ import { all } from "redux-saga/effects";
 import { watchFetchSuggestions } from "common/textInput/textInputSaga";
 import { watchFetchArticles } from "features/articleList/articleListSaga";
 import { watchFetchArticle } from "features/article/articleSaga";
+import { watchSearchHistoryChange } from "features/searchHistory/searchHistorySaga";
 
 export function* rootSaga() {
   yield all([
     watchFetchSuggestions(),
     watchFetchArticles(),
     watchFetchArticle(),
+    watchSearchHistoryChange(),
   ]);
 };
