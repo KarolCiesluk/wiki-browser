9dbcffd2a891bbf4c724878f1ad759136c4ae657
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-05 16:26:24 +0100

    export new components from common folder

--- src/common/index.js
+++ src/common/index.js
@@ -2,3 +2,7 @@ export { default as SearchForm } from "./SearchForm";
 export { default as inputValueName } from "./inputValueName";
 export { default as TextInput } from "./textInput/TextInput";
 export { default as PageTitle } from "./PageTitle";
+export { default as Wrapper } from "./Wrapper";
+export { default as CancelButton } from "./CancelButton";
+export { default as List } from "./List";
+export { default as Content } from "./Content";
