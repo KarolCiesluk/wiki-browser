4402a2efd34850ed02805521a591e7ca6e63d177
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-05 16:41:18 +0100

    simplify SearchHistory component

--- src/pages/searchHistory/SearchHistory/SearchHistory.js
+++ src/pages/searchHistory/SearchHistory/SearchHistory.js
@@ -1,70 +1,26 @@
-import { useSelector, useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
 
 import {
   selectIsHistoryEmpty,
-  selectFormattedDates,
-  clearHistory,
-  removeSelectedHistoryItems,
-  selectSelectedItemsCount,
-  unselectAllHistoryItems,
 } from "../searchHistorySlice";
-import { SearchListOfGivenDay } from "./SearchListOfGivenDay";
-import { PageTitle } from "common";
+import { PageTitle, Wrapper } from "common";
+import { Buttons } from "./Buttons";
+import { HistoryList } from "./HistoryList";
 
 const SearchHistory = () => {
   const isHistoryEmpty = useSelector(selectIsHistoryEmpty);
-  const formattedDates = useSelector(selectFormattedDates);
-  const selectedItemsCount = useSelector(selectSelectedItemsCount);
-
-  const dispatch = useDispatch();
-
-  const handleClearHistory = () => {
-    dispatch(clearHistory());
-  };
-
-  const handleRemoveSelected = () => {
-    dispatch(removeSelectedHistoryItems());
-  };
-
-  const handleUnselectAll = () => {
-    dispatch(unselectAllHistoryItems());
-  }
 
   return (
     <>
       {isHistoryEmpty ?
         <PageTitle>History is empty</PageTitle> :
         <>
-          <header>
-            <PageTitle>Search history</PageTitle>
+          <PageTitle>Search history</PageTitle>
 
-            <button onClick={handleClearHistory}>
-              Clear History
-            </button>
-
-            {!!selectedItemsCount &&
-              <>
-                <button onClick={handleUnselectAll}>
-                  X
-                </button>
-
-                <span>Selected: {selectedItemsCount}</span>
-
-                <button onClick={handleRemoveSelected}>
-                  Delete Selected
-                </button>
-              </>
-            }
-          </header>
-
-          <ul>
-            {formattedDates.map(day => (
-              <li key={nanoid()}>
-                <SearchListOfGivenDay day={day} />
-              </li>
-            ))}
-          </ul>
+          <Wrapper>
+            <Buttons />
+            <HistoryList />
+          </Wrapper>
         </>
       }
     </>
