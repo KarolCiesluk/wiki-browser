074f52f2a745bcb2293036650acd7ef966b6087e
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-05 16:22:18 +0100

    add margin to Footer

--- src/core/App/Layout/Footer/styled.js
+++ src/core/App/Layout/Footer/styled.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   border-top: 1px solid #EAEEF3;
+  margin-top: 40px;
+
+  @media(max-width: 500px) {
+    margin-top: 20px;
+  }
 `;
 
 export const Copyright = styled.p`
