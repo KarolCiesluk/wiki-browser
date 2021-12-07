e7482c1fd20a552d6e1a8b4a0b14806ecc94013d
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-07 10:42:56 +0100

    add ThemeProvider to index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Normalize } from "styled-normalize";
 import { Provider } from "react-redux";
+import { Normalize } from "styled-normalize";
+import { ThemeProvider } from "@mui/material";
 
-import App, { GlobalStyle, store } from "core";
+import App, { GlobalStyle, store, theme } from "core";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Normalize />
-      <GlobalStyle />
-      <App />
+      <ThemeProvider theme={theme}>
+        <Normalize />
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
