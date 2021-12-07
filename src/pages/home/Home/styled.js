a0ff0490a575f9399088889e23090d00c81f2e69
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-07 15:21:00 +0100

    delete Button from Home styling

--- src/pages/home/Home/styled.js
+++ src/pages/home/Home/styled.js
@@ -7,27 +7,3 @@ export const FormWrapper = styled(Wrapper)`
   grid-template-columns: 1fr;
   grid-gap: 14px;
 `;
-
-export const Button = styled.button`
-  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
-  padding: 16px;
-  border: 1px solid ${({ theme }) => theme.palette.primary.main};
-  font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-  font-size: 14px;
-  cursor: pointer;
-  color: ${({ theme }) => theme.palette.primary.main};
-  text-transform: uppercase;
-  background-color: unset;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.palette.action.hoverDenim};
-  }
-
-  &:active {
-    background-color: ${({ theme }) => theme.palette.action.activeDenim};
-  }
-
-  @media(max-width: ${({ theme }) => theme.breakpoints.values.sm}px) {
-    padding: 10px;
-  }
-`;
