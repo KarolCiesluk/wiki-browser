1664fe022a3d92f56a4c1d96b8f2fbba296b56fc
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-07 18:32:34 +0100

    export routing functions from core folder

--- src/core/index.js
+++ src/core/index.js
@@ -2,3 +2,4 @@ export { default } from "./App";
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as store } from "./store";
 export { theme } from "./theme";
+export { toHome, toArticle, toArticles, toHistory } from "./routes";
