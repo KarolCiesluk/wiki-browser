cc2622e120a41ab001344717627e7a4f59569176
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-07 18:31:20 +0100

    move hooks to other folder

--- src/pages/articles/Articles/useGoToPage.js
+++ src/pages/articles/Articles/useGoToPage.js
@@ -1,10 +1,11 @@
+import { pageQueryName } from "./pageQueryName";
 import { useReplaceQueryParameter } from "./useReplaceQueryParameter";
 
 export const useGoToPage = () => {
   const replaceQueryParameter = useReplaceQueryParameter();
 
   return (pageNumber) => replaceQueryParameter({
-    key: "page",
+    key: pageQueryName,
     value: pageNumber === 1 ? undefined : pageNumber,
   });
 };
