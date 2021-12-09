4df20dd16727e0a5a9397aac3d1cb8b956a5c632
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-09 18:20:22 +0100

    reduce top margin in Footer

--- src/core/App/Layout/Footer/styled.js
+++ src/core/App/Layout/Footer/styled.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   border-top: 1px solid ${({ theme }) => theme.palette.grey[200]};
-  margin-top: 40px;
+  margin-top: 20px;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.values.sm}px) {
-    margin-top: 20px;
+    margin-top: 14px;
   }
 `;
 
