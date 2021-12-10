cf27636ea4dc8349e155363350fe3d35af5440ac
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-10 12:34:33 +0100

    add signal argument in articleAPI

--- src/pages/article/articleAPI.js
+++ src/pages/article/articleAPI.js
@@ -1,18 +1,20 @@
 import { getArticleData } from "common/wikipediaAPI";
 
-export const getArticle = async ({ title, language }) => {
+export const getArticle = async ({ title, language, signal }) => {
   return await getArticleData({
     title,
     language,
     endpoint: "/bare",
+    signal,
   });
 };
 
-export const getArticleLanguages = async ({ title, language }) => {
+export const getArticleLanguages = async ({ title, language, signal }) => {
   return await getArticleData({
     title,
     language,
     endpoint: "/links/language",
     throwError: false,
+    signal,
   });
 };
