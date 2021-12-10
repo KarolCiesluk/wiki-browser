fad93d14b46d1a3b0032d87509407082f5b02914
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-10 12:17:21 +0100

    create useArticlePageState hook

---
+++ src/pages/article/Article/useArticlePageState.js
@@ -0,0 +1,46 @@
+import { defaultLanguageCode, useQueryParameter } from "common";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { getArticle, getArticleLanguages } from "../articleAPI";
+import { languageQueryName } from "./languageQueryName";
+
+export const useArticlePageState = () => {
+  const [status, setStatus] = useState("initial");
+  const [article, setArticle] = useState(null);
+  const [languages, setLanguages] = useState(null);
+
+  const { title } = useParams();
+  const language = useQueryParameter(languageQueryName) || defaultLanguageCode;
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    (async () => {
+      setStatus("loading");
+
+      try {
+        const [articleData, languagesData] = await Promise.all([
+          getArticle({ title, language, signal }),
+          getArticleLanguages({ title, language, signal }),
+        ]);
+
+        setLanguages(languagesData);
+        setArticle(articleData);
+        setStatus("success");
+      } catch (error) {
+        if (!signal.aborted) {
+          setStatus("error");
+          console.error("error");
+        }
+      }
+    })();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [title, language]);
+
+  return [status, article, languages];
+};
