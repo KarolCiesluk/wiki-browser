e6c37097c3a10472440323e85d66f5d1336ac0cd
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-12-10 12:16:18 +0100

    add props to LanguagesDrawer

--- src/pages/article/Article/LanguagesDrawer/LanguagesDrawer.js
+++ src/pages/article/Article/LanguagesDrawer/LanguagesDrawer.js
@@ -1,14 +1,8 @@
 import { nanoid } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
 import { IoClose } from "react-icons/io5";
 
 import { CancelButton } from "common";
-import {
-  selectArticleLanguages,
-  selectShowLanguages
-} from "../../articleSlice";
 import { goToOtherLanguage } from "./goToOtherLanguage";
-import { useToggleShowLanguages } from "../useToggleShowLanguages";
 import {
   LanguagesList,
   ListItem,
@@ -19,21 +13,17 @@ import {
   DrawerTitle
 } from "./styled";
 
-const LanguagesDrawer = () => {
-  const availableLanguages = useSelector(selectArticleLanguages);
-  const showLanguages = useSelector(selectShowLanguages);
-  const toggleShowLanguages = useToggleShowLanguages();
-
+const LanguagesDrawer = ({ showLanguages, availableLanguages, onClick }) => {
   return (
     <>
-      <Background open={showLanguages} onClick={toggleShowLanguages} />
+      <Background open={showLanguages} onClick={onClick} />
 
       <StyledDrawer active={showLanguages}>
 
         <DrawerHeader>
           <DrawerTitle>Read article in other language</DrawerTitle>
 
-          <CancelButton onClick={toggleShowLanguages}>
+          <CancelButton onClick={onClick}>
             <IoClose />
           </CancelButton>
 
